3b4ce8b08756adc31dce29941e798c5a8c736fee
Matheus Joranhezon <mjoranhezon@gmail.com>
2017-06-16 05:00:39 -0300

    Creating router for the angular aplication. Also created a simple html home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routes } from './app.router';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent} from '../pages/home/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
