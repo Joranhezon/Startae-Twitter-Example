388e6ce58be935df57dc068fc787804c2d69b1d1
Matheus Joranhezon <mjoranhezon@gmail.com>
2017-06-16 02:09:36 -0300

    Modifying schema so that the fields name and username are required to be filled.

--- server/schema/user.js
+++ server/schema/user.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  name: String,
-  twitter_username: String
+  name: { type: String, required: true },
+  twitter_username: { type: String, required: true }
 });
 
 module.exports = mongoose.model('User', UserSchema);
